b96cc63394508dcea52c94a48e71f3509d5c9759
pawel <p.dlugolecki93@gmail.com>
2024-02-08 20:45:38 +0100

    init commit

---
+++ src/main/java/com/app/MyPtojectApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPtojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPtojectApplication.class, args);
+	}
+
+}
